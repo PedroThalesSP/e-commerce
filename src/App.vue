5ac6fc48d465beeee9d32962143addc608c51f59
PedroThales <pedrothalessp@gmail.com>
2025-08-06 15:28:32 -0300

    estrutura base

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
-<template>
-  <v-app>
-    <v-main>
-      
-    </v-main>
-  </v-app>
-</template>
-
 <script setup lang="ts">
 </script>
+
+<template>
+  <v-container>
+    <v-app>
+      <v-app-bar>
+        <v-app-bar-title>
+          <p>a</p>
+        </v-app-bar-title>
+      </v-app-bar>
+    </v-app>
+    <div>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam eos laudantium cum repellat rem amet voluptatum distinctio odit aliquid, est culpa, harum veniam accusantium eum aut quas non ipsum minus.</p>
+    </div>
+  </v-container>  
+</template>
+
 
